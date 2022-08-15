216fd3e5af135bca8b7c42ed813952f0f814d89e
lompdoctor <pleclair@protonmail.com>
2022-08-15 11:49:10 -0500

    complete ex3.5

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = require('./persons.json')
+
+const generateId = (range) => {
+    const id = Math.floor(Math.random()*range)
+    if (persons.find(person => person.id === id)) {
+        id = Math.floor(Math.random()*range)
+    } else {
+        return id
+    }
+}
 
 app.get('/', (request, response) => {
     response.send(`<h1>G'day m8!</h1>`)
@@ -43,6 +55,25 @@ app.delete('/api/persons/:id', (request, response) => {
     console.log(persons)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const person = req.body
+    if (!person.name || !person.number) {
+        return res.status(400).json({
+            error: "name/number is missing!"
+        })
+    }
+    console.log(person)
+
+    const newPerson = {
+        id: generateId(1000),
+        name: person.name,
+        number: person.number
+    }
+
+    persons = persons.concat(newPerson)
+    res.json(newPerson)
 })
 
 app.get ('/api/persons', (request, response) => {
